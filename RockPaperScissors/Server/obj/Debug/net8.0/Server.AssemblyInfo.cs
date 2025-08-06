c5267d27aba8ef66dab401fde79e3f5ceaf9fb4e
huyvu0607 <vudh1092@ut.edu.vn>
2025-08-06 18:14:48 +0700

    tạo nhánh client

--- RockPaperScissors/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ RockPaperScissors/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22cef323d8eb64f1722fadb4e1e182192c3f8503")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
