d275099dd88a73e3a641797751ede7248fdb1e39
thanhtungdev05 <tungdt5101@ut.edu.vn>
2025-08-21 20:36:18 +0700

    upp winfrom va client

--- RockPaperScissors/Client/Form1.Designer.cs
+++ RockPaperScissors/Client/Form1.Designer.cs
@@ -28,90 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.btnReady = new System.Windows.Forms.Button();
             this.btnQuit = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnReady = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(107, 45);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 50);
+            this.button3.TabIndex = 19;
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(349, 59);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(405, 45);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 62);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Rock";
+            this.button1.Size = new System.Drawing.Size(89, 50);
+            this.button1.TabIndex = 18;
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(131, 59);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(268, 45);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 62);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Paper";
+            this.button2.Size = new System.Drawing.Size(75, 50);
+            this.button2.TabIndex = 17;
             this.button2.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(131, 178);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(515, 180);
-            this.listBox1.TabIndex = 4;
-            // 
-            // btnReady
-            // 
-            this.btnReady.BackColor = System.Drawing.Color.Lime;
-            this.btnReady.Location = new System.Drawing.Point(206, 362);
-            this.btnReady.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.btnReady.Name = "btnReady";
-            this.btnReady.Size = new System.Drawing.Size(105, 37);
-            this.btnReady.TabIndex = 8;
-            this.btnReady.Text = "Contrinue";
-            this.btnReady.UseVisualStyleBackColor = false;
-            this.btnReady.Visible = false;
             // 
             // btnQuit
             // 
             this.btnQuit.BackColor = System.Drawing.Color.LightCoral;
             this.btnQuit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnQuit.Location = new System.Drawing.Point(465, 362);
-            this.btnQuit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnQuit.Location = new System.Drawing.Point(358, 291);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(2);
             this.btnQuit.Name = "btnQuit";
-            this.btnQuit.Size = new System.Drawing.Size(105, 37);
-            this.btnQuit.TabIndex = 9;
+            this.btnQuit.Size = new System.Drawing.Size(79, 30);
+            this.btnQuit.TabIndex = 16;
             this.btnQuit.Text = "Quit";
             this.btnQuit.UseVisualStyleBackColor = false;
             this.btnQuit.Visible = false;
             // 
-            // button3
+            // btnReady
             // 
-            this.button3.Location = new System.Drawing.Point(543, 59);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(103, 62);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "Scissors";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnReady.BackColor = System.Drawing.Color.Lime;
+            this.btnReady.Location = new System.Drawing.Point(163, 291);
+            this.btnReady.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReady.Name = "btnReady";
+            this.btnReady.Size = new System.Drawing.Size(79, 30);
+            this.btnReady.TabIndex = 15;
+            this.btnReady.Text = "Contrinue";
+            this.btnReady.UseVisualStyleBackColor = false;
+            this.btnReady.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(107, 142);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(387, 147);
+            this.listBox1.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.btnQuit);
             this.Controls.Add(this.btnReady);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -121,12 +125,12 @@
 
         #endregion
 
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button btnReady;
         private System.Windows.Forms.Button btnQuit;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnReady;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- RockPaperScissors/RockPaperScissors/obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
+++ RockPaperScissors/RockPaperScissors/obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787f135b39ce556d1c4de07ba3aeaf3c2f0a5bec")]
 [assembly: System.Reflection.AssemblyProductAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
